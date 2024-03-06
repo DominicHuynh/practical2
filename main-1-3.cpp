3a5b2d549d99a74d09aeea991e416e9ec784207f
DominicHuynh <phucvbin2005@gmail.com>
2024-03-07 03:27:52 +1030

    1-3

---
+++ main-1-3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+extern void count_digits(int array[4][4]);
+int main(){
+int array[4][4]={{1,2,3,4},{1,2,3,4},{1,2,3,4},{1,2,3,4}};
+count_digits(array);
+
+
+
+
+
+    return 0;
+}
