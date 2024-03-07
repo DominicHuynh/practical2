c8ad910818b9b75c8991079bcc90c0971c36f151
DominicHuynh <phucvbin2005@gmail.com>
2024-03-07 14:24:33 +1030

    1-5

---
+++ function-1-5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+void print_summed(int array1[3][3],int array2[3][3]){
+    int sum_arr[3][3]={};
+    for (int i=0;i<3;i++){
+        for (int j=0;j<3;j++){
+            sum_arr[i][j]=array1[i][j]+array2[i][j];
+            std::cout<<sum_arr[i][j]<<" ";
+        }
+        std::cout<< std::endl;
+    }
+    std::cout<<std::endl;
+
+
+
+
+
+    return;
+}
